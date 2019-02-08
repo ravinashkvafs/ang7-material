f683b7a5457cbd85fbbae8de7a7af18849f99f13
Avinash Sharma <ravinashkvafs@gmail.com>
2019-02-08 21:44:50 +0530

    .

--- clientS/src/app/rest/rest.module.ts
+++ clientS/src/app/rest/rest.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { RestRoutingModule } from './rest-routing.module';
 import { OthersComponent } from './others/others.component';
 import { RestMaterialModule } from './rest-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [OthersComponent],
   imports: [
     CommonModule,
     RestRoutingModule,
-    RestMaterialModule
+    RestMaterialModule,
+    FlexLayoutModule
   ]
 })
 export class RestModule { }
