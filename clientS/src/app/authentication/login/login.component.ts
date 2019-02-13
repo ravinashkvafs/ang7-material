a1875707c19097ce9fdbb390a3ef031bb23e1270
Avinash Sharma <avinash@infield.co.in>
2019-02-13 16:00:33 +0530

    .

--- clientS/src/app/authentication/login/login.component.ts
+++ clientS/src/app/authentication/login/login.component.ts
@@ -14,14 +14,40 @@ export class LoginComponent implements OnInit {
       loginid: ['', [Validators.required, Validators.minLength(6)]],
       password: ['', [Validators.required, Validators.minLength(8)]]
     });
+
+    console.log(Object.assign({}, this.form.controls['loginid']));
+    // this.setValidators('loginid');
+    // setTimeout(() => {
+    //   this.removeValidators('loginid');
+    // }, 10000);
   }
 
-  email = new FormControl('', [Validators.required, Validators.email]);
+  getErrorMessage(key) {
+    console.log(key, this.form.controls[key].valid, this.form.controls[key].hasError('required'))
+    if (this.form.get(key).hasError('required'))
+      return `${key} is mandatory!`;
+    console.log('yaha')
+    if (this.form.controls[key].hasError('minLength'))
+      return `Kindly fulfill need for ${key}`;
+  }
 
-  getErrorMessage() {
-    return this.email.hasError('required') ? 'You must enter a value' :
-      this.email.hasError('email') ? 'Not a valid email' :
-        '';
+  validateValue(key) {
+    if (this.form.controls[key].hasError('required'))
+      return true;
+    else if (this.form.controls[key].hasError('minLength'))
+      return true;
+
+    return false;
+  }
+
+  setValidators(key) {
+    this.form.controls[key].setValidators([Validators.required, Validators.minLength(3)]);
+    this.form.controls[key].updateValueAndValidity();
+  }
+
+  removeValidators(key) {
+    this.form.controls[key].clearValidators();
+    this.form.controls[key].updateValueAndValidity();
   }
 
   //   onChanges() {
