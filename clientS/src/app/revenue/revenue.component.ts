34385255618c180cf00717d83f3af39a55c4ec92
Avinash Sharma <ravinashkvafs@gmail.com>
2019-03-18 22:21:01 +0530

    working on revenue

--- clientS/src/app/revenue/revenue.component.ts
+++ clientS/src/app/revenue/revenue.component.ts
@@ -12,5 +12,10 @@ export class RevenueComponent implements OnInit {
   ngOnInit() {
   }
 
-  tab_colors = ['tab1', 'tab2', 'tab1'];
+  tabs: Array<object> = [
+    { icon: 'pie_chart', title: 'Total Remaining' },
+    { icon: 'monetization_on', title: 'Total Spent' },
+    { icon: 'cloud', title: 'Total Expenditure' }
+  ];
+  tab_colors: Array<string> = ['tab1', 'tab2', 'tab1'];
 }
