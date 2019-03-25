4f22cbcc5c4cb069d3e43db3fafb7b6691519626
Avinash Sharma <ravinashkvafs@gmail.com>
2019-03-25 22:55:21 +0530

    events + refinements

--- clientS/src/app/events/events.component.ts
+++ clientS/src/app/events/events.component.ts
@@ -9,19 +9,40 @@ export class EventsComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  list: any; newLI: any;
+
+  ngOnInit() { }
 
   events: any = [
-    { name: 'Avinash Sharma', statement: '2 Year(s) Anniversary', date_type: 'normal', date: '2017-03-17' },
+    { name: 'Avinash Sharma', statement: '2 Year(s) Work Anniversary', date_type: 'normal', date: '2017-03-17' },
     { name: 'Rohit Sharma', statement: 'Birth Anniversary', date_type: 'normal', date: '1995-11-27' },
-    { name: 'Amit Dhiman', statement: '10 Year(s) Anniversary', date_type: 'normal', date: '2009-06-09' }
+    { name: 'Amit Dhiman', statement: '10 Year(s) Work Anniversary', date_type: 'normal', date: '2009-06-09' },
+    { name: 'Rohit Sharma', statement: 'Birth Anniversary', date_type: 'normal', date: '1995-11-27' },
+    { name: 'Amit Dhiman', statement: '10 Year(s) Work Anniversary', date_type: 'normal', date: '2009-06-09' },
+    { name: 'Rohit Sharma', statement: 'Birth Anniversary', date_type: 'normal', date: '1995-11-27' },
+    { name: 'Amit Dhiman', statement: '10 Year(s) Work Anniversary', date_type: 'normal', date: '2009-06-09' },
+    { name: 'Rohit Sharma', statement: 'Birth Anniversary', date_type: 'normal', date: '1995-11-27' },
+    { name: 'Amit Dhiman', statement: '10 Year(s) Work Anniversary', date_type: 'normal', date: '2009-06-09' },
+    { name: 'Rohit Sharma', statement: 'Birth Anniversary', date_type: 'normal', date: '1995-11-27' },
+    { name: 'Amit Dhiman', statement: '10 Year(s) Work Anniversary', date_type: 'normal', date: '2009-06-09' },
+    { name: 'Rohit Sharma', statement: 'Birth Anniversary', date_type: 'normal', date: '1995-11-27' },
+    { name: 'Amit Dhiman', statement: '10 Year(s) Work Anniversary', date_type: 'normal', date: '2009-06-09' }
   ];
 
   months: Array<string> = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
   giveMeDateMonth(date) {
     let t = date.split('-');
-    return { d: t[2], m: this.months[parseInt(t[1])] };
+    return { d: t[2], m: this.months[parseInt(t[1])], y: t[0] };
+  }
+
+  add() {
+    this.list = document.getElementById('list');
+    this.newLI = document.createElement('li');
+    this.newLI.innerHTML = 'A new item';
+    this.list.appendChild(this.newLI);
+    setTimeout(function () {
+      this.newLI.className = this.newLI.className + " show";
+    }, 10);
   }
 }
